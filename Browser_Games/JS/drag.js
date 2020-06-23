ad86aca5a1559f197cf1ebe37ce7740852a424f7
zachk00 <1800firstlast8001@gmail.com>
2020-06-23 17:26:41 -0400

    first commit

---
+++ Browser_Games/JS/drag.js
@@ -0,0 +1,16 @@
+
+var id;
+
+
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+ id = ev.target.id;
+}
+
+function drop(ev) {
+  
+  ev.target.append(document.getElementById(id));
+}
