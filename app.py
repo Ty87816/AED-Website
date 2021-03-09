f54198cc8f76d837571cccafcd16da54a3001afb
Zachary Kerr <zacharykerr@ufl.edu>
2021-03-08 21:44:43 -0500

    Update app.py
    connected backend to the frontend of login/registration
    
    currently only includes registration

--- app.py
+++ app.py
@@ -10,13 +10,14 @@ def hello_world():
     title = "PDSP Home"
     return render_template('index.html', title=title)
     
-@app.route('/login')
+    
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
-        
-@app.route('/register')
-def register():
-    return render_template('register.html')
+    reg_form = RegistrationForm()
+    log_form = LoginForm()
+    if reg_form.validate_on_submit()
+        flash('f'Succes for {form.username.data}!','success')
+    return render_template('login.html', form=reg_form)
     
 if __name__ == '__main__':
     app.run()
