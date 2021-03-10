fa6e151159d610ccdfc5e5a713fd1ead905d8cfb
Zachary Kerr <zacharykerr@ufl.edu>
2021-03-09 21:07:55 -0500

    Update app.py
    - Added routes for event and comment wall page
    - Fixed typos

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
-from flask import request, render_template
+from flask import request, render_template, url_for, flash, redirect
 import json
-
+from forms import LoginForm, RegistrationForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'testing_key' 
 
 @app.route('/')
 def hello_world():
@@ -17,6 +18,14 @@ def login():
     if reg_form.validate_on_submit():
         flash(f'Succes for {reg_form.username.data}!','success')
     return render_template('login.html', form=reg_form)
+
+@app.route('/events')
+def events():
+    return render_template('Events.html')
+
+@app.route('/comments')
+def comments():
+    return render_template('CommentWall.html')
     
 if __name__ == '__main__':
     app.run()
