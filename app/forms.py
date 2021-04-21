a2d0e320b275206d09017497303fb5f17072eb55
Zachary Kerr <zacharykerr@ufl.edu>
2021-04-21 10:53:49 -0400

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+
 from app.models import User
+
+
+
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
@@ -26,4 +31,9 @@ class LoginForm(FlaskForm):
 	password = PasswordField('Password', validators=[DataRequired()])
 	submit = SubmitField('Sign In')
 	
-	
+    
+class AttendanceForm(FlaskForm):
+    f_name = StringField('First Name', validators=[DataRequired()])
+    l_name = StringField('Last Name', validators=[DataRequired()])
+    doc = FileField('Upload Document', validators=[FileRequired(), FileAllowed(['png', 'jpeg','jpg','docx'], 'PNG, JPG, JPEG, or DOCX Document only!')])
+    submit = SubmitField('Submit Attendance')
