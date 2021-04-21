5ad784535c8e5c8f39ebfed694e8559ccd6db346
Zachary Kerr <zacharykerr@ufl.edu>
2021-04-21 10:54:57 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,8 +5,10 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'testing_key' 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db' 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Flask_Cen@localhost/AED' 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 from app import routes
